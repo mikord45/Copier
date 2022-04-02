2350463f427a23ff71180a7acee38ced2caf4a1c
mikord45 <51155742+mikord45@users.noreply.github.com>
2022-04-03 00:20:17 +0200

    Copier class started

---
+++ Zadanie1/Copier.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ver1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public int PrintCounter = 0;
+
+        public int ScanCounter = 0;
+        public void Print(in IDocument document)
+        {
+            Console.WriteLine(DateTime.Now.ToString() + " Print: " + document.GetFileName());
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType)
+        {
+            Console.WriteLine(formatType);
+            switch (formatType)
+            {
+                case IDocument.FormatType.PDF:
+                    Console.WriteLine(DateTime.Now.ToString() + " Scan: " + "PDFScan" + Counter + ".pdf");
+                    document = new PDFDocument("PDFScan" + Counter + ".pdf");
+                    break;
+                case IDocument.FormatType.JPG:
+                    Console.WriteLine(DateTime.Now.ToString() + " Scan: " + "ImageScan" + Counter + ".jpg");
+                    document = new ImageDocument("ImageScan" + Counter + ".jpg");
+                    break;
+                case IDocument.FormatType.TXT:
+                    Console.WriteLine(DateTime.Now.ToString() + " Scan: " + "TextScan" + Counter + ".txt");
+                    document = new TextDocument("TextScan" + Counter + ".txt");
+                    break;
+                default:
+                    document = null;
+                    break;
+            }
+        }
+
+        public void Scan(out IDocument document)
+        {
+            Console.WriteLine(DateTime.Now.ToString() + " Scan: " + "ImageScan" + Counter + ".jpg");
+            document = new ImageDocument("ImageScan" + Counter + ".jpg");
+        }
+
+        public void ScanAndPrint()
+        {
+            IDocument doc = new ImageDocument("image.img");
+            Scan(out doc);
+            Print(doc);
+        }
+    }
+}
--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 
-namespace Zadanie1
+namespace ver1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Copier copier = new Copier();
+            IDocument document = new PDFDocument("aaa.pdf");
+            copier.Print(document);
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            IDocument doc2 = new ImageDocument("img.img");
+            copier.Scan(out doc1, doc1.GetFormatType());
+            copier.Scan(out doc2);
             Console.WriteLine("Hello World!");
         }
     }
